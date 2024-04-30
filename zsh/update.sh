c83b4f24555a22b92df81cf23a99ec9f940f8653
nickyfr33ze <nickyfr33ze26@pm.me>
2024-04-30 08:34:39 -0700

    Altered the order of the zsh/update.sh script for better transparency.

--- zsh/update.sh
+++ zsh/update.sh
@@ -1,13 +1,21 @@
 echo "[*] Running updates..."
 sleep 1
-echo "[*] Running: 'sudo apt update && sudo apt upgrade -y'"
+echo "[*] Running: 'sudo apt update'"
 sleep 0.5
 echo "[*] If you are not 'root' please input your password."
-sudo apt update && sudo apt upgrade -y
+sudo apt update
 sleep 0.5
 echo "------------------------"
 sleep 1
 echo "[*] Updates complete."
+echo "------------------------"
+sleep 1
+echo "[*] Running: sudo apt upgrade -y"
+sleep 0.5
+sudo apt upgrade -y
+sleep 1
+echo "------------------------"
+echo "[*] Upgrades complete."
 echo "------------------------"
 sleep 1
 echo "[*] Running: 'sudo apt autoremove -y'"
