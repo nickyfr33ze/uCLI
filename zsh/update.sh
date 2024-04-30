d429148208e9e1beb86054f1115c71e4b1a0e8e2
nickyfr33ze <nickyfr33ze26@pm.me>
2024-04-30 09:12:49 -0700

    Altered timing of program.

--- zsh/update.sh
+++ zsh/update.sh
@@ -1,17 +1,17 @@
 echo "[*] Running updates..."
 sleep 1
 echo "[*] Running: 'sudo apt update'"
-sleep 0.5
+sleep 1
 echo "[*] If you are not 'root' please input your password."
 sudo apt update
-sleep 0.5
+sleep 1
 echo "------------------------"
 sleep 1
 echo "[*] Updates complete."
 echo "------------------------"
 sleep 1
 echo "[*] Running: sudo apt upgrade -y"
-sleep 0.5
+sleep 1
 sudo apt upgrade -y
 sleep 1
 echo "------------------------"
@@ -20,7 +20,7 @@ echo "------------------------"
 sleep 1
 echo "[*] Running: 'sudo apt autoremove -y'"
 sudo apt autoremove -y
-sleep 0.5
+sleep 1
 echo "--------------------------------------------"
 sleep 1
 echo "[*] Applications autoremoved successfully."
