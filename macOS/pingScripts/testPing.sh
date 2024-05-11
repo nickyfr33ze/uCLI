8a761a6958901f91e63627e094400322e377e098
Nick Friesen <nickyfr33ze26@pm.me>
2024-05-11 15:56:45 -0700

    Added clarification on executing scirpt 'testPing.sh'

--- macOS/pingScripts/testPing.sh
+++ macOS/pingScripts/testPing.sh
@@ -4,7 +4,11 @@ echo "[*] Testing Connectivity."
 sleep 1
 echo "How many ping(s) would you like to send?"
 read value
+sleep 1
 echo "--------------------------"
+sleep 1
+echo "[*] Executing 'ping -c $value google.com"
+echo ""
 sleep 1
 ping -c $value google.com
 
