073f11ce718812401a0e1699b68f6374361a6d2e
Nick Friesen <nickyfr33ze26@pm.me>
2024-05-16 19:09:57 -0700

    Started a netdiscover script on Linux machines

---
+++ Linux/netdiscover.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Created by Nick Friesen on 2024-05-16 @ 19:05
+# This script is to scan the network the linux machine is connected to currently.
+# This should be a quicker snapshot of the network.
+# Another option is to include flags within the script that makes it go on longer.
+
+
