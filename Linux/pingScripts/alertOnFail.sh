ec9ea3e38bc4e9aa34f5d20eac39cfe5ad93b896
Nick Friesen <nickyfr33ze26@pm.me>
2024-07-10 10:39:03 -0700

    Altered to have user input the desired IP

--- Linux/pingScripts/alertOnFail.sh
+++ Linux/pingScripts/alertOnFail.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# IP address to ping
-IP_ADDRESS="192.168.1.1"
+# Ask the user to input the IP address
+read -p "Enter the IP address to ping: " IP_ADDRESS
 
 # Ping the IP address with a timeout of 1 second and 4 attempts, and make it verbose
 ping -c 6 -W 1 -v $IP_ADDRESS
-# > /dev/null 2>&1
+
 # Check the exit status of the ping command
 if [ $? -ne 0 ]; then
     echo "Failed to connect to $IP_ADDRESS after 4 attempts."
