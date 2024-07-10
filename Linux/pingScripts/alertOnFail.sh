5a035d42ec04ede72c094075a013e3c78b0cb303
Nick Friesen <nickyfr33ze26@pm.me>
2024-07-10 10:44:13 -0700

    Added keyboard interruption and formatting

--- Linux/pingScripts/alertOnFail.sh
+++ Linux/pingScripts/alertOnFail.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
+# Function to handle keyboard interruption
+interrupt_handler() {
+    echo "Script interrupted. Exiting..."
+    break
+}
+
+# Set the interrupt_handler function to be executed on SIGINT
+trap interrupt_handler SIGINT
+
 # Ask the user to input the IP address
 read -p "Enter the IP address to ping: " IP_ADDRESS
+echo "----------------------------------------"
 
-# Ping the IP address with a timeout of 1 second and 4 attempts, and make it verbose
-ping -c 6 -W 1 -v $IP_ADDRESS
+# Run the ping command indefinitely until interrupted
+while true; do
+    # Ping the IP address with a timeout of 1 second and 4 attempts, and make it verbose
+    ping $IP_ADDRESS
 
-# Check the exit status of the ping command
-if [ $? -ne 0 ]; then
-    echo "Failed to connect to $IP_ADDRESS after 4 attempts."
-    exit 1
-fi
+    # Check the exit status of the ping command
+    if [ $? -ne 0 ]; then
+        echo "Failed to connect to $IP_ADDRESS after 4 attempts."
+        exit 1
+    fi
 
-echo "Successfully connected to $IP_ADDRESS."
+    echo "Successfully connected to $IP_ADDRESS."
+done
