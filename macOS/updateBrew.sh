636580e17c6ad9c711ca60685b3499f348df43c9
Nick Friesen <nickyfr33ze26@pm.me>
2024-09-24 21:16:37 -0700

    Updated macOS Homebrew update script

--- macOS/updateBrew.sh
+++ macOS/updateBrew.sh
@@ -1,2 +1,16 @@
+echo [*] Running 'brew update && brew upgrade'
+sleep 1
+echo ---------------------------------
+sleep 1
 brew update && brew upgrade
-
+sleep 1
+echo ---------------------------------
+sleep 1
+echo [*] Updates pulled and upgrades installed
+sleep 1
+echo ---------------------------------
+sleep 1
+echo [*] TERMINATING PROGRAM
+sleep 1
+echo ---------------------------------
+sleep 1
