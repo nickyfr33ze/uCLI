38be94e4e33f4e871c44efe13be9f5a1bc2f2900
NickyFr33ze <nickyfr33ze26@pm.me>
2024-10-26 17:32:58 -0700

    Small refactor

--- macOS/updateBrew.sh
+++ macOS/updateBrew.sh
@@ -6,11 +6,11 @@ brew update && brew upgrade
 sleep 1
 echo ---------------------------------
 sleep 1
-echo [*] Updates pulled and upgrades installed
+echo [*] Updates pulled and Upgrades installed
 sleep 1
 echo ---------------------------------
 sleep 1
-echo [*] TERMINATING PROGRAM
+echo [!] TERMINATING PROGRAM
 sleep 1
 echo ---------------------------------
 sleep 1
