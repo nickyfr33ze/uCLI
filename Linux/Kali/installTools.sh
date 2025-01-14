1c217da9199fcca03327eaded905e779733600a1
Nick Friesen <nickyfr33ze26@pm.me>
2025-01-13 20:49:51 -0800

    Added a space for Kali script development

---
+++ Linux/Kali/installTools.sh
@@ -0,0 +1,19 @@
+echo "This will install ALL of kali's tools, and some cool wallpapers. Are you sure you want to do this?"
+#sudo apt install kali-wallpapers-all kali-tweaks kali-tools-*
+echo "Would you like to install 'pimpmykali'?"
+# git clone https://github.com/Dewalt-arch/pimpmykali.git
+# I want to ask the user if they want to install the 'pimpmykali' repo and run the script within.
+# CODE BELOW IS THE WORK IN PROGRESS
+# read -p "Do you want to install 'pimpmykali'?" -n 1 -r
+# echo   # (optional) move to a new line
+# if [[ $REPLY =~ ^[Yy]$ ]]
+# then
+    # git clone https://github.com/Dewalt-arch/pimpmykali.git
+    # cd pimpmykali
+    # chmod +x pimpmykali.sh
+    # ./pimpmykali.sh
+# fi
+# else 
+    # echo "You have chosen not to install 'pimpmykali'."
+# fi
+# read -p "Do you want to install 'kali-anonsurf'?" -n 1 -r
